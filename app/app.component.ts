1f32fbd0bb2e5c40cc93ef301e88f8adfb57d4fd
Nick Mead <nick@>
2017-03-28 16:30:51 -0700

    start basic app setup

---
+++ app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>Tap Room</h1>
+  `
+})
+
+export class AppComponent {
+  kegs: Keg[] = [
+    new Keg("Keg 1", "Budweiser", 100, 5.6),
+    new Keg("Keg 2", "Heineken", 120, 6.1),
+    new Keg("Keg 3", "PBR", 95, 4.8)
+  ];
+}
+
+export class Keg {
+  constructor(public name: string, public brand: string, public price: int, public alcoholContent: double) { }
+}
